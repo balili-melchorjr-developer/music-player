63ec64b3f32cffa4600a37abed6daa02b2f17b17
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-02-05 06:46:43 +0800

    Added Project models

--- api/models.py
+++ api/models.py
@@ -19,3 +19,16 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Project(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    comments = models.CharField(max_length=500, blank=True, null=True)
+    status = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
