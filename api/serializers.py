e37d47a382694abf2982db22966c1b0aade5927a
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-02-07 08:40:28 +0800

    Changes are related to React Forms

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room, Project
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
         fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, ProjectViewset
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('room', RoomView.as_view())
-]
+
+router = DefaultRouter()
+router.register('project', ProjectViewset, basename='project')
+urlpatterns = router.urls
+
+
+
+# urlpatterns = [
+#     path('room', RoomView.as_view())
+# ]
 
